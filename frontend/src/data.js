1d7fa5645acd5dda2dcbc5a044d9d4e8de60f6cf
Kamil Ilyas <kamililyas@gmail.com>
2022-10-28 19:07:37 +0500

    view and search property

---
+++ frontend/src/data.js
@@ -0,0 +1,121 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/img6.jpg';
+import img7 from './img/img7.jpg';
+import img8 from './img/img8.jpg';
+import img9 from './img/img9.jpg';
+import img10 from './img/img10.jpg';
+import img11 from './img/img11.jpg';
+import img12 from './img/img12.jpg';
+import img13 from './img/img13.jpg';
+import img14 from './img/img14.jpg';
+
+const data = {
+    cardData: [
+        {
+            id:1,
+            img: img1,
+            title: 'house along road',
+            desc: 'Rs. 1 Crore'
+        },
+
+        {
+            id:2,
+            img: img2,
+            title: 'house with garden',
+            desc: 'Rs. 3 Crore'
+        },
+
+        {
+            id:3,
+            img: img3,
+            title: 'unique house',
+            desc: 'Rs. 4 Crore'
+        },
+
+        {
+            id:4,
+            img: img4,
+            title: 'house along road',
+            desc: 'Rs. 4 Crore'
+        },
+
+        {
+            id:5,
+            img: img5,
+            title: 'house along road',
+            desc: 'Rs. 5 Crore'
+        },
+
+        {
+            id:6,
+            img: img6,
+            title: 'house with pool',
+            desc: 'Rs. 6 Crore'
+        },
+
+        {
+            id:7,
+            img: img7,
+            title: 'house with pool',
+            desc: 'Rs. 4 Crore'
+        },
+
+        {
+            id:8,
+            img: img8,
+            title: 'house with pool',
+            desc: 'Rs. 7 Crore'
+        },
+
+        {
+            id:9,
+            img: img9,
+            title: 'unique house',
+            desc: 'Rs. 1 Crore'
+        },
+
+        {
+            id:10,
+            img: img10,
+            title: 'house with garden',
+            desc: 'Rs. 5 Crore'
+        },
+
+        {
+            id:11,
+            img: img11,
+            title: 'house with pool',
+            desc: 'Rs. 2 Crore'
+        },
+
+        {
+            id:12,
+            img: img12,
+            title: 'unique house',
+            desc: 'Rs. 6 Crore'
+        },
+
+        {
+            id:13,
+            img: img13,
+            title: 'house with pool',
+            desc: 'Rs. 3 Crore'
+        },
+
+        {
+            id:14,
+            img: img14,
+            title: 'unique house',
+            desc: 'Rs. 8 Crore'
+        }
+
+
+    ]
+}
+
+
+export default data;
